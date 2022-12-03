8362c1a3deb798fb5b10f85fcb10740bfeb643fe
jiepengtan <jiepengtan@gmail.com>
2022-12-03 14:51:39 +0800

    [+] 修正初始化事件

---
+++ Assets/Scripts/Game.View/Manager/Main.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace GamesTan.Game.View {
+    public class Main : MonoBehaviour {
+        public GameObject Root;
+        private void Awake() { 
+            EventUtil.RemoveAllListener();
+            Root.SetActive(true);
+        }
+    }
+}
